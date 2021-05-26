77344d20b08a775068ecc11aceeb632ad73fa332
ybadmus <yusif.badmus@gmail.com>
2021-05-26 19:55:32 +0100

    add back button function to app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import './reset.css';
 import './style.css';
 
 import getWeather from './modules/apiModule';
-import { validateInput } from './modules/helper'
+import { validateInput, clearResultView } from './modules/helper'
 
 const loadListerners = () => {
     const search_btn_listener = document.getElementById('search-btn');
     search_btn_listener.addEventListener('click', function() {
-        const city = document.getElementById('search-input').value.trim();
+        const city = document.getElementById('search-input').value.trim().toLowerCase();
         validateInput(city) ? getWeather(city) : console.log('Invalid entry');
+    });
+
+    const backBtn_listener = document.getElementById('back-btn');
+    backBtn_listener.addEventListener('click', function() {
+        clearResultView();
     });
 };
 
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -5,19 +5,16 @@ const validateInput = (name) => {
 const renderResultView = (weather) => {
     if (weather) {
 
-        console.log(weather);
-
         document.getElementById('result-temp').textContent = weather.main.temp;
         document.getElementById('result-place').textContent = `${weather.name}, ${weather.sys.country}`;
         document.getElementById('result-weather').textContent = weather.weather[0].main;
         document.getElementById('result-weather-desc').textContent = weather.weather[0].description;
 
-        document.getElementById('result-feeling').textContent = '';
-        document.getElementById('result-humidity').textContent = '';
-        document.getElementById('result-pressure').textContent = '';
-        document.getElementById('result-mintemp').textContent = '';
-        document.getElementById('result-maxtemp').textContent = '';
-
+        document.getElementById('result-feeling').textContent = weather.main.feels_like;
+        document.getElementById('result-humidity').textContent = weather.main.humidity;
+        document.getElementById('result-pressure').textContent = weather.main.pressure;
+        document.getElementById('result-mintemp').textContent = weather.main.temp_min;
+        document.getElementById('result-maxtemp').textContent = weather.main.temp_max;
 
         const mainView = document.getElementById('mainView');
         mainView.style.display = 'none';
@@ -27,7 +24,28 @@ const renderResultView = (weather) => {
 
         document.getElementById('search-input').value = '';
     }
+}
+
+const clearResultView = () => {
+
+    document.getElementById('result-temp').textContent = '';
+    document.getElementById('result-place').textContent = '';
+    document.getElementById('result-weather').textContent = '';
+    document.getElementById('result-weather-desc').textContent = '';
+
+    document.getElementById('result-feeling').textContent = '';
+    document.getElementById('result-humidity').textContent = '';
+    document.getElementById('result-pressure').textContent = '';
+    document.getElementById('result-mintemp').textContent = '';
+    document.getElementById('result-maxtemp').textContent = '';
+
+    const mainView = document.getElementById('mainView');
+    mainView.style.display = 'block';
+
+    const resultView = document.getElementById('resultView');
+    resultView.style.display = 'none';
 
 }
 
-export { validateInput, renderResultView }
+
+export { validateInput, renderResultView, clearResultView }

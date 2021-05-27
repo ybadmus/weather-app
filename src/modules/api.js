0e04851b4397845fe5755324e415e86f3c5b50b8
ybadmus <yusif.badmus@gmail.com>
2021-05-27 13:33:44 +0100

    add prompt for handling errors when entry invalid

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,9 +1,18 @@
-const getWeather = async (city) => {
-  const response = await fetch(`${process.env.BASE_URL}?q=${city}&appid=${process.env.API_KEY}`, { mode: 'cors' });
-  const weather = await response.json();
-  localStorage.setItem('weather-info', JSON.stringify(weather));
-  localStorage.setItem('weather-unit', 'fahrenheit');
-  return weather;
+const getWeather = async(city) => {
+    try {
+        const response = await fetch(`${process.env.BASE_URL}?q=${city}&appid=${process.env.API_KEY}`, { mode: 'cors' });
+        if (response.status >= 404) {
+            const err = await response.json();
+            throw err;
+        }
+        const weather = await response.json();
+        localStorage.setItem('weather-info', JSON.stringify(weather));
+        localStorage.setItem('weather-unit', 'fahrenheit');
+        return weather;
+    } catch (error) {
+        throw new Error(error)
+    }
+
 };
 
 export default getWeather;

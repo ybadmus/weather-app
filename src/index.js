c94fd3d3c9c0274346a55e379afe0825583266e7
ybadmus <yusif.badmus@gmail.com>
2021-05-27 04:42:34 +0100

    fix a few bug, app logic complete

--- src/index.js
+++ src/index.js
@@ -6,88 +6,117 @@ import darkNight from './the-dark-night-wallpaper.jpg';
 
 import getWeather from './modules/api';
 import {
-  validateInput,
-  loadResults,
-  clearDetailsView,
-  convertTempFahrenheit,
-  toggleMainView,
-  toggleDetailsView,
+    validateInput,
+    loadResults,
+    clearDetailsView,
+    convertTempFahrenheit,
+    toggleMainView,
+    toggleDetailsView,
+    convertFromKelvinToCelsius
 } from './modules/helper';
 
 const loadListerners = () => {
-  const searchBtnlistener = document.getElementById('search-btn');
-  searchBtnlistener.addEventListener('click', () => {
-    const loader = document.getElementById('loader');
-    loader.className = 'loader loader-default is-active';
+    const searchBtnlistener = document.getElementById('search-btn');
+    searchBtnlistener.addEventListener('click', () => {
+        const loader = document.getElementById('loader');
+        loader.className = 'loader loader-default is-active';
 
-    const userInput = document.getElementById('search-input').value.trim();
-    if (validateInput(userInput)) {
-      getWeather(userInput).then((resp) => {
-        loadResults(resp);
-        toggleMainView();
-        document.getElementById('search-input').value = '';
-        loader.className = 'loader loader-default';
-      })
-        .catch((error) => {
-          loader.className = 'loader loader-default';
-          throw error;
-        });
-    } else {
-      loader.className = 'loader loader-default';
-    }
-  });
+        const userInput = document.getElementById('search-input').value.trim();
+        if (validateInput(userInput)) {
+            getWeather(userInput).then((resp) => {
+                    const keys = Object.keys(resp);
 
-  const backBtnListener = document.getElementById('back-btn');
-  backBtnListener.addEventListener('click', () => {
-    clearDetailsView();
-    toggleDetailsView();
-  });
+                    for (let prop = 0; prop < keys.length; prop += 1) {
+                        if (keys[prop] === 'main') {
+                            resp.main.temp = convertFromKelvinToCelsius(resp[keys[prop]].temp);
+                            resp.main.feels_like = convertFromKelvinToCelsius(resp[keys[prop]].feels_like);
+                            resp.main.humidity = resp[keys[prop]].humidity;
+                            resp.main.pressure = resp[keys[prop]].pressure;
+                            resp.main.temp_min = convertFromKelvinToCelsius(resp[keys[prop]].temp_min);
+                            resp.main.temp_max = convertFromKelvinToCelsius(resp[keys[prop]].temp_max);
+                        }
+                    }
 
-  const changeUnitListener = document.getElementById('change-unit-btn');
-  changeUnitListener.addEventListener('click', () => {
-    if (localStorage.getItem('weather-unit') === 'fahrenheit') {
-      const weather = JSON.parse(localStorage.getItem('weather-info'));
-      const unitButton = document.getElementById('change-unit-btn');
-      unitButton.textContent = 'C °';
-      const tempUnit = document.getElementById('temp-unit');
-      tempUnit.textContent = 'F';
-
-      const keys = Object.keys(weather);
-
-      for (let prop = 0; prop < keys.length; prop += 1) {
-        if (keys[prop] === 'main') {
-          weather.main.temp = convertTempFahrenheit(weather[keys[prop]].temp);
-          weather.main.feels_like = convertTempFahrenheit(weather[keys[prop]].feels_like);
-          weather.main.humidity = convertTempFahrenheit(weather[keys[prop]].humidity);
-          weather.main.pressure = convertTempFahrenheit(weather[keys[prop]].pressure);
-          weather.main.temp_min = convertTempFahrenheit(weather[keys[prop]].temp_min);
-          weather.main.temp_max = convertTempFahrenheit(weather[keys[prop]].temp_max);
+                    loadResults(resp);
+                    toggleMainView();
+                    document.getElementById('search-input').value = '';
+                    loader.className = 'loader loader-default';
+                })
+                .catch((error) => {
+                    loader.className = 'loader loader-default';
+                    throw error;
+                });
+        } else {
+            loader.className = 'loader loader-default';
         }
-      }
+    });
 
-      localStorage.setItem('weather-unit', 'celsius');
-      loadResults(weather);
-    } else if (localStorage.getItem('weather-unit') === 'celsius') {
-      const weather = JSON.parse(localStorage.getItem('weather-info'));
-      const unitButton = document.getElementById('change-unit-btn');
-      unitButton.textContent = 'F °';
-      const tempUnit = document.getElementById('temp-unit');
-      tempUnit.textContent = 'C';
+    const backBtnListener = document.getElementById('back-btn');
+    backBtnListener.addEventListener('click', () => {
+        clearDetailsView();
+        toggleDetailsView();
+    });
 
-      localStorage.setItem('weather-unit', 'fahrenheit');
-      loadResults(weather);
-    }
-  });
+    const changeUnitListener = document.getElementById('change-unit-btn');
+    changeUnitListener.addEventListener('click', () => {
+        if (localStorage.getItem('weather-unit') === 'fahrenheit') {
+
+            const weather = JSON.parse(localStorage.getItem('weather-info'));
+            const unitButton = document.getElementById('change-unit-btn');
+            unitButton.textContent = 'Change Unit (C °)';
+            const tempUnit = document.getElementById('temp-unit');
+            tempUnit.textContent = 'F';
+
+            const keys = Object.keys(weather);
+
+            for (let prop = 0; prop < keys.length; prop += 1) {
+                if (keys[prop] === 'main') {
+                    weather.main.temp = convertTempFahrenheit(weather[keys[prop]].temp);
+                    weather.main.feels_like = convertTempFahrenheit(weather[keys[prop]].feels_like);
+                    weather.main.humidity = convertTempFahrenheit(weather[keys[prop]].humidity);
+                    weather.main.pressure = convertTempFahrenheit(weather[keys[prop]].pressure);
+                    weather.main.temp_min = convertTempFahrenheit(weather[keys[prop]].temp_min);
+                    weather.main.temp_max = convertTempFahrenheit(weather[keys[prop]].temp_max);
+                }
+            }
+
+            localStorage.setItem('weather-unit', 'celsius');
+            loadResults(weather);
+        } else if (localStorage.getItem('weather-unit') === 'celsius') {
+
+            const weather = JSON.parse(localStorage.getItem('weather-info'));
+            const unitButton = document.getElementById('change-unit-btn');
+            unitButton.textContent = 'Change Unit (F °)';
+            const tempUnit = document.getElementById('temp-unit');
+            tempUnit.textContent = 'C';
+
+            const keys = Object.keys(weather);
+
+            for (let prop = 0; prop < keys.length; prop += 1) {
+                if (keys[prop] === 'main') {
+                    weather.main.temp = convertFromKelvinToCelsius(weather[keys[prop]].temp);
+                    weather.main.feels_like = convertFromKelvinToCelsius(weather[keys[prop]].feels_like);
+                    weather.main.humidity = weather[keys[prop]].humidity;
+                    weather.main.pressure = weather[keys[prop]].pressure;
+                    weather.main.temp_min = convertFromKelvinToCelsius(weather[keys[prop]].temp_min);
+                    weather.main.temp_max = convertFromKelvinToCelsius(weather[keys[prop]].temp_max);
+                }
+            }
+
+            localStorage.setItem('weather-unit', 'fahrenheit');
+            loadResults(weather);
+        }
+    });
 };
 
 const main = () => {
-  document.body.style.background = `url(${darkNight})`;
+    document.body.style.background = `url(${darkNight})`;
 
-  const mainView = document.getElementById('mainView');
-  mainView.style.display = 'flex';
+    const mainView = document.getElementById('mainView');
+    mainView.style.display = 'flex';
 
-  const resultView = document.getElementById('resultView');
-  resultView.style.display = 'none';
+    const resultView = document.getElementById('resultView');
+    resultView.style.display = 'none';
 };
 
 main();
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,56 +1,59 @@
 const validateInput = (name) => name !== '';
 
 const loadResults = (weather) => {
-  if (weather) {
-    document.getElementById('result-temp').textContent = `Temp: ${weather.main.temp} °`;
-    document.getElementById('result-place').textContent = `${weather.name}, ${weather.sys.country}`;
-    document.getElementById('result-weather').textContent = weather.weather[0].main;
-    document.getElementById('result-weather-desc').textContent = weather.weather[0].description;
+    if (weather) {
+        document.getElementById('result-temp').textContent = `Temp: ${weather.main.temp} °`;
+        document.getElementById('result-place').textContent = `${weather.name}, ${weather.sys.country}`;
+        document.getElementById('result-weather').textContent = weather.weather[0].main;
+        document.getElementById('result-weather-desc').textContent = weather.weather[0].description;
 
-    document.getElementById('result-feeling').textContent = weather.main.feels_like;
-    document.getElementById('result-humidity').textContent = weather.main.humidity;
-    document.getElementById('result-pressure').textContent = weather.main.pressure;
-    document.getElementById('result-mintemp').textContent = weather.main.temp_min;
-    document.getElementById('result-maxtemp').textContent = weather.main.temp_max;
-  }
+        document.getElementById('result-feeling').textContent = weather.main.feels_like;
+        document.getElementById('result-humidity').textContent = weather.main.humidity;
+        document.getElementById('result-pressure').textContent = weather.main.pressure;
+        document.getElementById('result-mintemp').textContent = weather.main.temp_min;
+        document.getElementById('result-maxtemp').textContent = weather.main.temp_max;
+    }
 };
 
 const clearDetailsView = () => {
-  document.getElementById('result-temp').textContent = '';
-  document.getElementById('result-place').textContent = '';
-  document.getElementById('result-weather').textContent = '';
-  document.getElementById('result-weather-desc').textContent = '';
+    document.getElementById('result-temp').textContent = '';
+    document.getElementById('result-place').textContent = '';
+    document.getElementById('result-weather').textContent = '';
+    document.getElementById('result-weather-desc').textContent = '';
 
-  document.getElementById('result-feeling').textContent = '';
-  document.getElementById('result-humidity').textContent = '';
-  document.getElementById('result-pressure').textContent = '';
-  document.getElementById('result-mintemp').textContent = '';
-  document.getElementById('result-maxtemp').textContent = '';
+    document.getElementById('result-feeling').textContent = '';
+    document.getElementById('result-humidity').textContent = '';
+    document.getElementById('result-pressure').textContent = '';
+    document.getElementById('result-mintemp').textContent = '';
+    document.getElementById('result-maxtemp').textContent = '';
 };
 
 const toggleMainView = () => {
-  const mainView = document.getElementById('mainView');
-  mainView.style.display = 'none';
+    const mainView = document.getElementById('mainView');
+    mainView.style.display = 'none';
 
-  const resultView = document.getElementById('resultView');
-  resultView.style.display = 'block';
+    const resultView = document.getElementById('resultView');
+    resultView.style.display = 'block';
 };
 
 const toggleDetailsView = () => {
-  const mainView = document.getElementById('mainView');
-  mainView.style.display = 'block';
+    const mainView = document.getElementById('mainView');
+    mainView.style.display = 'block';
 
-  const resultView = document.getElementById('resultView');
-  resultView.style.display = 'none';
+    const resultView = document.getElementById('resultView');
+    resultView.style.display = 'none';
 };
 
-const convertTempFahrenheit = (temp) => Math.round((1.8 * Number(temp) + 32) * 100) / 100;
+const convertTempFahrenheit = (temp) => Math.round((1.8 * Number(temp - 273.15) + 32) * 100) / 100;
+
+const convertFromKelvinToCelsius = (temp) => Math.round((temp - 273.15) * 100) / 100;
 
 export {
-  validateInput,
-  loadResults,
-  clearDetailsView,
-  convertTempFahrenheit,
-  toggleMainView,
-  toggleDetailsView,
+    validateInput,
+    loadResults,
+    clearDetailsView,
+    convertTempFahrenheit,
+    toggleMainView,
+    toggleDetailsView,
+    convertFromKelvinToCelsius
 };
